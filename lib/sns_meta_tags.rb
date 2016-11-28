4afe39a1f2f8a8dec886fb2f232f1790761579a7
matsuhisa <matsuhisa+github@gmail.com>
2016-11-29 00:20:02 +0900

    Modified

--- lib/sns_meta_tags.rb
+++ lib/sns_meta_tags.rb
@@ -4,12 +4,12 @@ require 'nokogiri'
 class SnsMetaTags
   attr_accessor :url, :user_agent
 
-  GOOGLE_BOT_SMARTPHONE_USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+  GOOGLE_BOT_SMARTPHONE = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 
-  def initialize(url, user_agent = GOOGLE_BOT_SMARTPHONE_USER_AGENT)
+  def initialize(url, user_agent = GOOGLE_BOT_SMARTPHONE)
     @url = url
     @user_agent = user_agent
-    page_read(@url, @user_agent)
+    page_read
   end
 
   def foo
@@ -25,16 +25,20 @@ class SnsMetaTags
   end
 
   def canonical
-    @doc.xpath('//link[@rel="canonical"]').attribute('href').value
+    @doc.xpath('//link[@rel="canonical"]').attribute('href').value if @doc && @doc.xpath('//link[@rel="canonical"]').size.nonzero?
   end
 
-  def page_read(url, user_agent)
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset
-      f.read
+  private
+  def page_read(url = @url, user_agent = GOOGLE_BOT_SMARTPHONE)
+    begin
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      @doc = Nokogiri::HTML.parse(html, nil, charset)
+    rescue OpenURI::HTTPError
     end
-    @doc = Nokogiri::HTML.parse(html, nil, charset)
   end
 
 end
--- spec/lib/sns_meta_tags_spec.rb
+++ spec/lib/sns_meta_tags_spec.rb
@@ -1,18 +1,38 @@
 require "spec_helper"
 require "sns_meta_tags"
 
+require 'webmock/rspec'
+include WebMock::API
+WebMock.allow_net_connect!
+
 describe "SnsMetaTags" do
+  describe "#page_read" do
+    subject { SnsMetaTags.new(url).send(:page_read) }
+    let(:url) { 'http://www.excale.com/' }
+
+    context 'When status 404' do
+      before { stub_request(:any, url).to_return(status: 404) }
+      it { is_expected.to be_nil }
+    end
+  end
+
   describe "#canonical" do
     subject { SnsMetaTags.new(url).canonical }
+    let(:url) { 'http://www.excale.com/' }
 
-    context 'When http://hatenablog.com/' do
-      let(:url) { 'http://hatenablog.com/' }
+    context 'When status 200 and have canonical' do
+      before {
+        stub_request(:any, url).to_return(
+          status: 200,
+          body: "<link href='#{url}' rel='canonical'>"
+        )
+      }
       it { is_expected.to eq url }
     end
 
-    context 'When duplicate m (http://hatenablog.comm/)' do
-      let(:url) { 'http://hatenablog.comm/' }
-      it { is_expected.not_to eq url }
+    context 'When status 200 and no canonical' do
+      before { stub_request(:any, url).to_return(status: 200) }
+      it { is_expected.to eq nil }
     end
 
   end
