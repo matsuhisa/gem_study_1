37e5fa09fc383dd4e91def1544368ef54c5a5627
matsuhisa <matsuhisa+github@gmail.com>
2016-12-01 00:01:09 +0900

    Add og:image test

--- spec/lib/sns_meta_tags_spec.rb
+++ spec/lib/sns_meta_tags_spec.rb
@@ -34,6 +34,40 @@ describe "SnsMetaTags" do
       before { stub_request(:any, url).to_return(status: 200) }
       it { is_expected.to eq nil }
     end
+  end
 
+  describe "#og_images" do
+    subject { SnsMetaTags.new(url).og_images }
+    let(:url) { 'http://www.excale.com/' }
+
+    context 'When status 200 and have og:image' do
+      html_string = <<"EOS"
+      <meta property="og:image" content="http://www.excale.com/1.png">
+      <meta property="og:image" content='http://www.excale.com/2.png'>
+EOS
+
+      before {
+        stub_request(:any, url).to_return(
+          status: 200,
+          body: html_string
+        )
+      }
+      it { is_expected.not_to contain_exactly('http://www.excale.com/1.png') }
+      it { is_expected.not_to contain_exactly('http://www.excale.com/2.png') }
+      it { is_expected.to contain_exactly('http://www.excale.com/1.png', 'http://www.excale.com/2.png') }
+    end
+
+    context 'When status 200 and no have og:image' do
+      subject { SnsMetaTags.new(url).og_images }
+      let(:url) { 'http://www.excale.com/' }
+
+      before {
+        stub_request(:any, url).to_return(
+            status: 200,
+            body: ""
+        )
+      }
+      it { is_expected.to contain_exactly() }
+    end
   end
 end
